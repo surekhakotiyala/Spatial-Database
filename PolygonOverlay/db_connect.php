4665fcd41d9bbcaa76a54ddab4a4df08e4d4cdec
skotiyala <skotiyala@cs2.mwsu.edu>
2014-06-23 13:26:21 -0500

    Updating Military-installations

---
+++ PolygonOverlay/db_connect.php
@@ -0,0 +1,26 @@
+<?php
+
+/* This file connects to your MySQL database. */
+
+// Connection constants 
+define('DB_HOST',     'localhost');
+define('DB_USER',     'skotiyala');
+define('DB_PASSWORD', 'Yi0keek6');
+define('DB_DATABASE', 'skotiyala');
+define('DB_PORT',     3306);
+
+// Error-checking to ensure you have set the values above before trying to run one of the examples
+if(DB_USER     === 'REPLACEME' ||
+    DB_PASSWORD === 'REPLACEME' ||
+    DB_DATABASE === 'REPLACEME')
+{
+    die("You must set your database connection variables in db_connect.php before viewing this example!");
+}
+
+// Connect to the database using PHP's MySQLi object-oriented library
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT);
+if ($mysqli->connect_errno)
+{
+    die("Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error);
+}
+
