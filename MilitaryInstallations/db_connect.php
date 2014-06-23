eb1c1b32cab4e905ad8a8a35907ec5ba8d828045
skotiyala <skotiyala@cs2.mwsu.edu>
2014-06-23 12:59:14 -0500

    Updating Assignment 3

---
+++ MilitaryInstallations/db_connect.php
@@ -0,0 +1,11 @@
+
+<?php
+//Establish connection to database.
+$db = new mysqli('localhost', 'skotiyala', 'Yi0keek6', 'skotiyala');
+
+//If no connection, then kill page
+if($db->connect_errno > 0){
+    die('Unable to connect to database [' . $db->connect_error . ']');
+}
+?>
+<?php
